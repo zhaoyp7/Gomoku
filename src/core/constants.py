460c746ec01699dcd1fd9bc5f2e8d4a20983e205
zhaoyp <zhaoyp7@sjtu.edu.cn>
2025-10-14 13:47:13 +0800

    init

---
+++ src/core/constants.py
@@ -0,0 +1,12 @@
+# 游戏当中的一些常量定义，请不要随便更改
+# 在文件之前加入 from .constants import * 就可以直接使用
+
+BOARD_SIZE = 15
+EMPTY = 0
+BLACK = 1
+WHITE = 2
+
+PLAYING = 0
+BLACK_WIN = 1
+WHITE_WIN = 2
+DRAW = 3
---
+++ src/core/game.py
@@ -0,0 +1,18 @@
+# 需要实现游戏的基本逻辑判断
+
+from .board import Board
+from .constants import *
+
+# 实现一个 Game 类，维护以下信息
+# size : 棋盘大小
+# board : 当前棋盘
+# game_state : 当前游戏状态
+
+# 实现以下函数
+# __init__(self) : 初始化
+# MakeMove(self, row, col) : 调用 Board 实现落子，并且判断落子后游戏状态
+# PrintBoard(self) : 输出棋盘
+
+
+# 目前只要实现简单的输出
+# 之后需要添加可视化的部分
