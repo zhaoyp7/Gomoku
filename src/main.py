368a93dc5b7dd095bb22334913f1175e7d6211d9
zhaoyp <zhaoyp7@sjtu.edu.cn>
2025-10-16 20:30:43 +0800

    update

--- src/main.py
+++ src/main.py
@@ -10,14 +10,16 @@ def main() :
     game = Game()
     ai = RandomAI()
     while game.game_state == PLAYING : 
-        print("请输入落子位置")
-        row, col = map(int,input().split())
-        while not(game.board.check_valid_move(row,col )) :
-            print("落子无效，请重新输入：")
+        if game.board.current_player == BLACK :
+            print("请输入落子位置")
             row, col = map(int,input().split())
-        game.make_move(row, col)
-        pos = ai.GetMove(game.board)
-        game.make_move(pos[0], pos[1])
+            while not(game.board.check_valid_move(row,col )) :
+                print("落子无效，请重新输入：")
+                row, col = map(int,input().split())
+            game.make_move(row, col)
+        else :
+            pos = ai.GetMove(game.board)
+            game.make_move(pos[0], pos[1])
 
 if __name__ == "__main__":
     main()
